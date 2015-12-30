104e1d0f9cb3ca7cf91e48d55b15a3c71b0deae2
Fabio Rocha <fabio@fabiofarias.com.br>
2015-12-30 15:26:48 -0200

    First

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
+        <link rel="Stylesheet" type="text/css" href="croppie/demo/prism.css" />
+        <link rel="Stylesheet" type="text/css" href="croppie/bower_components/sweetalert/dist/sweetalert.css" />
+        <link rel="Stylesheet" type="text/css" href="croppie/croppie.css" />
+        <link rel="Stylesheet" type="text/css" href="croppie/demo/demo.css" />
+        <link rel="icon" href="//foliotek.github.io/favico-64.png" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="grid">
+                    <div class="col-1-2">
+                        <h1>Croppie</h1>
+                        <p>
+                            Croppie is a fast, easy to use image cropping plugin with tons of configuration options!
+                        </p>
+                        <div class="buttons">
+                            <a href="https://github.com/foliotek/croppie" class="btn">View on Github</a>
+                            <button class="js-main-image">Get Result</button>
+                        </div>
+                    </div>
+                    <div class="col-1-2">
+                        <div class="demo-cropper" id="cropper-1">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="title">Laravel 5x</div>
+            </div>
+        </div>
+    </body>
+</html>
